c5a009a7285d5aa00b8058512d38e7991f6be61c
macos <macos@macoss-MacBook-Pro.local>
2021-08-14 15:21:32 +0530

    intro and portfolio added

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,37 @@
-import './intro.scss';
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
+export default function Intro() {
+  const textRef = useRef();
 
-export default function Intro(){
-    return(
-        <div className="intro" id="intro">
-            intro
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay:1500,
+      backSpeed:60,
+      strings: ["Mobile Developer", "Front-End Developer","Backend Developer"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imageContainer">
+          <img src="assets/man.png" alt="" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Ankit Metwal</h1>
+          <h3>
+             <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,65 @@
-import './portfolio.scss';
+import { useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
+import "./portfolio.scss";
+import {
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+  featuredPortfolio,
+} from "../../data";
+import { useEffect } from "react";
 
-export default function Portfolio(){
-    return(
-        <div className="portfolio" id="portfolio">
-            portfolio
-        </div>
-    )
+export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    { id: "featured", title: "Featured" },
+    { id: "web", title: "Web App" },
+    { id: "mobile", title: "Mobile App" },
+    { id: "design", title: "Design" },
+    { id: "content", title: "Content" },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+    }
+  }, [selected]);
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            setSelected={setSelected}
+            active={selected === item.id}
+            {...item}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        {data.map((item, index) => (
+          <div key={item.id} className="item">
+            <img src={item.img} alt="" />
+            <h3>{item.title}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
